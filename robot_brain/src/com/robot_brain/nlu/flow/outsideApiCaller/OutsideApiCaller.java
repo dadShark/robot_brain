8db97cf195a4904e6a489d9e8db4fd9228e671b5
钱磊 <lqian_knowology@163.com>
2020-03-15 17:48:52 +0800

    新增：流程规则执行代码

--- robot_brain/src/com/robot_brain/nlu/flow/outsideApiCaller/OutsideApiCaller.java
+++ robot_brain/src/com/robot_brain/nlu/flow/outsideApiCaller/OutsideApiCaller.java
@@ -1,15 +1,13 @@
-package com.robot_brain.nlu.process.outsideApiCaller;
+package com.robot_brain.nlu.flow.outsideApiCaller;
 
 import com.robot_brain.nlu.myInterface.StandardModule;
 
 
 public class OutsideApiCaller implements StandardModule {
-    @Override
     public void _init_() {
 
     }
 
-    @Override
     public String _main_(String conversationID) {
         return null;
     }
